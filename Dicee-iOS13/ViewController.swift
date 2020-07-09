5fdc5ef124d496cbdc70efdf00e9e3d29f1b0a9d
Leticia Cosin <leticia.cosin@hotmail.com>
2020-07-09 18:27:26 -0300

    first commit - adicionado imagens e trocar components

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBOutlet allows me to reference a UI Element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        diceImageView1.alpha = 0.5
+        
+        //WHO           WHAT    VALUE
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
         // Do any additional setup after loading the view.
     }
 
